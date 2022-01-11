e3a58c437737c03423197dcf630a188aafb58ab0
areddy1 <AnuragReddy@rocketmortgage.com>
2022-01-11 09:20:19 -0500

    second commit

--- RoomCalculator.js
+++ RoomCalculator.js
@@ -1,38 +1,47 @@
-function areaOfRectangle() {
+function GetRoomCalculation() {
 
 
-    let rectangleLength = parseInt(prompt("Enter length of rectangle:")); 
-    let rectangleWidth = parseInt(prompt("Enter width of rectangle:")); 
-    
+    let rectangleLength = parseFloat(prompt("Enter length of rectangle:")); 
+    let rectangleWidth = parseFloat(prompt("Enter width of rectangle:")); 
+    let height = parseFloat(prompt("Enter the height:")); 
+
     let areaOfRectangle = rectangleLength * rectangleWidth; 
-    
+     let perimeterOfRectangle = 2 * (rectangleLength + rectangleWidth); 
+     let volume = rectangleLength * rectangleWidth * height; 
+     let paint = perimeterOfRectangle / 5; 
+     let carpet = areaOfRectangle / 5; 
     console.log("Area of rectangle is: " + areaOfRectangle); 
+    console.log("Perimeter of rectangle is: " + perimeterOfRectangle);
+    console.log("Volume of room is " + volume); 
+    console.log("You will need " + paint + " cans of paint to fill this room.");
+    console.log("You will need " + carpet + " squares of carpet to fill this room.");
+
     }
 
-function perimeterOfRectangle() {
+// function perimeterOfRectangle() {
 
-    let rectangleLength = parseInt(prompt("Enter length of rectangle:")); 
-    let rectangleWidth = parseInt(prompt("Enter width of rectangle:")); 
+//     let rectangleLength = parseInt(prompt("Enter length of rectangle:")); 
+//     let rectangleWidth = parseInt(prompt("Enter width of rectangle:")); 
     
-    let perimeterOfRectangle = 2 * (rectangleLength + rectangleWidth); 
+//     let perimeterOfRectangle = 2 * (rectangleLength + rectangleWidth); 
     
-    console.log("Perimeter of rectangle is: " + perimeterOfRectangle);
-}
+//     console.log("Perimeter of rectangle is: " + perimeterOfRectangle);
+// }
 
-function paint(p) {
-    return p /5;  
+// function paint(p) {
+//     return p /5;  
     
-}
+// }
 
-function carpet(a) {
-    return a /5; 
-}
+// function carpet(a) {
+//     return a /5; 
+// }
 
-function RoomCalculation() {
-let neededPaint = paint(p);
-let neededCarpet = carpet(a);
+// function RoomCalculation() {
+// let neededPaint = paint(p);
+// let neededCarpet = carpet(a);
 
-console.log("You will need " + neededPaint + " cans of paint to fill this room.")
-console.log("You will need " + neededCarpet + " squares of carpet to fill this room.")
+// console.log("You will need " + neededPaint + " cans of paint to fill this room.")
+// console.log("You will need " + neededCarpet + " squares of carpet to fill this room.")
 
-}
+// }
